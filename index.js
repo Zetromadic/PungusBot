1c6aa66cd1acff039b372d0af20b891f0f727068
Zetromadic <58966456+Zetromadic@users.noreply.github.com>
2020-08-24 03:45:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 //const token = process.env.token;
-const token = ''
+const token = 'NzI4MDQ3Mzk2OTUyNDczNjI4.Xv0s-Q.drajw-d1FgVnUOrCn6HixeBoZ-g'
 
 const PREFIX = 'p!';
 
